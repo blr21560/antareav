eea33e0641b0c930b1206be027c47f06474ffc45
blr21560 <lr.b21560@gmail.com>
2022-01-15 10:41:25 +0100

    add proxy module 2

--- middleware/liste_especes.js
+++ middleware/liste_especes.js
@@ -3,7 +3,8 @@ export default async function({ store, $axios }) {
     console.log("middleware liste espece");
     if (!store.getters['liste_especes/isload']) {
         console.log("middleware liste espece apres test");
-        let ret = await $axios.$get("http://antarea.fr/iden/esp");
+        //let ret = await $axios.$get("http://antarea.fr/iden/esp");
+        let ret = await $axios.$get("/api/esp");
         console.log("middleware liste espece apres axios");
         console.log("middleware liste espece ret");
         //console.log(ret);
